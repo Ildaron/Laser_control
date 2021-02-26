b53c2e522eb004a0790d38e0cef92a49c7415952
Ildar <de.ikar@yandex.ru>
2021-02-26 10:32:43 +0900

    Update 0.take_pthoto.py

--- 6.Stereo_vision/Calibration/0.take_pthoto.py
+++ 6.Stereo_vision/Calibration/0.take_pthoto.py
@@ -9,7 +9,7 @@ for a in range (0,30,1):
  ret, frame = cap.read()
  #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/left/'+str(a)+'.jpg',frame)
- cap.release()
- cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 
