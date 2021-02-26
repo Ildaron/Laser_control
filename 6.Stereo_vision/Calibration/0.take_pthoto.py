278a4fc46c912c6bf24249ca45635ddf7d8a0997
Ildar <de.ikar@yandex.ru>
2021-02-26 11:47:44 +0900

    Update 0.take_pthoto.py

--- 6.Stereo_vision/Calibration/0.take_pthoto.py
+++ 6.Stereo_vision/Calibration/0.take_pthoto.py
@@ -3,13 +3,21 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import time
-cap = cv2.VideoCapture(1)
+
 for a in range (0,30,1):
  time.sleep(1)
+ cap = cv2.VideoCapture(2)         
  ret, frame = cap.read()
- #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/left/'+str(a)+'.jpg',frame)
-cap.release()
+ cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/right/'+'img'+str(a)+'.png',frame)
+ cap = cv2.VideoCapture(1)         
+ ret, frame = cap.read()
+ cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/left/'+'img'+str(a)+'.png',frame)
+
+
+
 cv2.destroyAllWindows()
+
+
+
 
 
